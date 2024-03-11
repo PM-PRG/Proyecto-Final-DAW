d052ee8bdb576164b7e66f07dfbf871f70329a86
PM-PRG <130345386+PM-PRG@users.noreply.github.com>
2024-03-11 14:02:27 +0100

    formulario login

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciar sesion</title>
+    <link rel="stylesheet" href="css/login.css">
+</head>
+<body>
+    
+    
+        <div class="log-form">
+        <!-- formulario de inicio de sesión -->
+        <h2>Login to your account</h2>
+        <form>
+        <input type="text" title="username" placeholder="username" />
+        <input type="password" title="password" placeholder="password" />
+        <button type="submit" class="btn">Login</button>
+        <a class="forgot" href="#">Forgot Username?</a>
+        </form>
+    </div>
+    
+    
+    
+    
+</body>
+</html>
